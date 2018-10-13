8db1c7f33e82dc6de28a01fac232386e733bd46d
Victoria Allen <vickmae.allen@gmail.com>
2018-10-12 18:03:34 -0700

    Finished Shop Page

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-console.log('Keep moving forward!')
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
